07b7697d1cec849771ad0f190d86e0b8bbedaf36
junho-lee <18098199+Kyzarusty@users.noreply.github.com>
2022-10-04 16:47:50 +1300

    planning for app

---
+++ main.js
@@ -0,0 +1,6 @@
+// Create 2 boxes for input and output
+// Create dropdown list
+// Import API for currency exchange rates
+// Be able to select items from dropdown list
+// If you insert number in input box, output box displays correctly converted number
+// Both boxes can be used as input/output box
