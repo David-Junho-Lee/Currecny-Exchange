5497b5eb7411beb72975d3d8eb78b04d167d4d1b
junho-lee <18098199+Kyzarusty@users.noreply.github.com>
2022-10-04 22:25:09 +1300

    ccomplete

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let currencyRatio = {
     AUD: 1.54,
     NZD: 1.75,
     KRW: 1432.44,
-    unit: 'Dollars',
+    unit: 'Dollar',
   },
   KRW: {
     USD: 0.0007,
@@ -25,21 +25,21 @@ let currencyRatio = {
     AUD: 1,
     NZD: 1.13,
     KRW: 928.26,
-    unit: 'Dollars',
+    unit: 'Dollar',
   },
   NZD: {
     USD: 0.57,
     AUD: 0.88,
     NZD: 1,
     KRW: 818.16,
-    unit: 'Dollars',
+    unit: 'Dollar',
   },
 }
 
 let fromCurrency = 'NZD'
 let toCurrency = 'USD'
 
-document.querySelectorAll('#from-currency-list li').forEach((menu) =>
+document.querySelectorAll('#from-currency-list p').forEach((menu) =>
   menu.addEventListener('click', function () {
     // get button
     // change value in button
@@ -52,7 +52,7 @@ document.querySelectorAll('#from-currency-list li').forEach((menu) =>
   })
 )
 
-document.querySelectorAll('#to-currency-list li').forEach((menu) =>
+document.querySelectorAll('#to-currency-list p').forEach((menu) =>
   menu.addEventListener('click', function () {
     document.getElementById('to-btn').textContent = this.textContent
     toCurrency = this.textContent
@@ -63,18 +63,33 @@ document.querySelectorAll('#to-currency-list li').forEach((menu) =>
 )
 
 // when you type numbers in from box, curreny exchange happens and display value
-function fromConvert() {
-  // get user input from-input-box
-  let fromAmount = document.getElementById('from-input').value
-  // get converted value
-  let convertedAmount = fromAmount * currencyRatio[fromCurrency][toCurrency]
-  // display converted value in 'input-box'
-  document.getElementById('to-input').value = convertedAmount
-}
+// function fromConvert() {
+//   // get user input from-input-box
+//   let fromAmount = document.getElementById('from-input').value
+//   // get converted value
+//   let convertedAmount = fromAmount * currencyRatio[fromCurrency][toCurrency]
+//   // display converted value in 'input-box'
+//   document.getElementById('to-input').value = convertedAmount
+// }
 
-// do the same for to-input-box
-function toConvert() {
-  let toAmount = document.getElementById('to-input').value
-  let toFromConvertedAmount = toAmount * currencyRatio[toCurrency][fromCurrency]
-  document.getElementById('from-input').value = toFromConvertedAmount
+// // do the same for to-input-box
+// function toConvert() {
+//   let toAmount = document.getElementById('to-input').value
+//   let toFromConvertedAmount = toAmount * currencyRatio[toCurrency][fromCurrency]
+//   document.getElementById('from-input').value = toFromConvertedAmount
+// }
+
+// refactor
+
+function convert(type) {
+  let amount = 0
+  if (type == 'from') {
+    amount = document.getElementById('from-input').value
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency]
+    document.getElementById('to-input').value = convertedAmount
+  } else {
+    amount = document.getElementById('to-input').value
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency]
+    document.getElementById('from-input').value = convertedAmount
+  }
 }
